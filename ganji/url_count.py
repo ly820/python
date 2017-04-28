9556516947f8f18df08fc5156da7e7fe1ab26dfc
liuyuan <820668452@qq.com>
2017-04-28 17:14:15 +0800

    移除python下所有文件

--- ganji/url_count.py
+++
@@ -1,6 +0,0 @@
-from save_commodity_url import commodity_urls
-import time
-
-while True:
-    print(commodity_urls.find().count())
-    time.sleep(3)
--- tongcheng_58/url_count.py
+++
@@ -1,6 +0,0 @@
-import time
-from  page_prasing import url_list
-
-while True:
-    print(url_list.find().count())
-    time.sleep(5)
